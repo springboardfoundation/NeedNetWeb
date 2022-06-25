ba684e02571e5c80992c356cf37d22f16176bbb2
Abu Ubada <abcoding17@gmail.com>
2022-06-25 17:33:09 +0530

    feat: add login form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 
-
 const Home = () => {
   return (
     <Link to='/login' > Login</Link >
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <form action="#" method="POST" autoComplete="on">
+        <h1>Sign in</h1>
+
+        <label>
+          Mobile Number&nbsp;
+          <input type="tel" pattern="[6-9]{1}\d{9}" required placeholder="ex: 98765432210" title="Please add a valid 10-digit Mobile Number" maxLength="10" />
+        </label>
+        <br />
+        <br />
+        <label>
+          Password&nbsp;
+          <input type="text" required />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Sign in" />
+        <p>
+          Don't have an account? <Link to="#" >Sign Up</Link>
+        </p>
+      </form>
+    </div>
   )
 }
 
